61e3bc2934a91a0bf5a430fbeaf9f35ed9d1841f
Eugene <levgenii.rachkov@gettyimages.com>
2021-01-28 00:44:57 +0200

    Init commit

---
+++ src/bot.py
@@ -0,0 +1,10 @@
+import pyautogui
+pyautogui.moveTo(400, 60)
+pyautogui.hotkey('Win')
+pyautogui.PAUSE = 5
+# pyautogui.typewrite('Opera')
+# pyautogui.keyDown('Enter')
+# pyautogui.PAUSE = 5
+# pyautogui.moveTo(400, 60)
+
+
